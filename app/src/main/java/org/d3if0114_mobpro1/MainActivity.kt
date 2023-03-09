f74368c105b076c59dcf52190f8f503b6467c7f9
Abdul Mughny Raihan <ramughny@student.telkomuniversity.ac.id>
2023-03-09 16:17:50 +0700

    3.2.Menambahkan view binding

--- app/src/main/java/org/d3if0114_mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if0114_mobpro1/MainActivity.kt
@@ -2,10 +2,23 @@ package org.d3if0114_mobpro1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.d3if0114_mobpro1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener { hitungBmi() }
+    }
+
+    private fun hitungBmi() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
