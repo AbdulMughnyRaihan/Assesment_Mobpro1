da0acd1dc19741539f80dba28129d86c4adf2a27
Abdul Mughny Raihan <ramughny@student.telkomuniversity.ac.id>
2023-04-07 02:07:38 +0700

    Memisahkan UI code dan logic hitung BMI

--- app/src/main/java/org/d3if0114_mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if0114_mobpro1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
 import org.d3if0114_mobpro1.databinding.ActivityMainBinding
+import org.d3if0114_mobpro1.model.HasilBmi
 import org.d3if0114_mobpro1.model.KategoriBmi
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +18,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener { hitungBmi() }
+        binding.button.setOnClickListener { hitungBmii() }
     }
 
-    private fun hitungBmi() {
+    private fun hitungBmii() {
         val berat = binding.beratEditText.text.toString()
         if (TextUtils.isEmpty(berat)) {
             Toast.makeText(this, R.string.berat_invalid, Toast.LENGTH_LONG).show()
@@ -31,19 +32,25 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
             return
         }
-        val tinggiCm = tinggi.toFloat() / 100
 
         val selectedId = binding.radioGroup.checkedRadioButtonId
         if (selectedId == -1) {
             Toast.makeText(this, R.string.gender_invalid, Toast.LENGTH_LONG).show()
             return
         }
-        val isMale = selectedId == R.id.priaRadioButton
-        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
-        val kategori = getKategori(bmi, isMale)
+        val result = hitungBmi(
+            berat.toFloat(),
+            tinggi.toFloat(),
+            selectedId == R.id.priaRadioButton
+        )
+        showResult(result)
+    }
 
-        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
-        binding.kategoriTextView.text = getString(R.string.kategori_x, getKategoriLabel(kategori))
+    private fun hitungBmi(berat: Float, tinggi: Float, isMale: Boolean): HasilBmi {
+        val tinggiCm = tinggi / 100
+        val bmi = berat / (tinggiCm * tinggiCm)
+        val kategori = getKategori(bmi, isMale)
+        return HasilBmi(bmi, kategori)
     }
 
     private fun getKategori(bmi: Float, isMale: Boolean): KategoriBmi {
@@ -62,6 +69,7 @@ class MainActivity : AppCompatActivity() {
         }
         return kategori
     }
+
     private fun getKategoriLabel(kategori: KategoriBmi): String {
         val stringRes = when (kategori) {
             KategoriBmi.KURUS -> R.string.kurus
@@ -70,4 +78,10 @@ class MainActivity : AppCompatActivity() {
         }
         return getString(stringRes)
     }
+    private fun showResult(result: HasilBmi) {
+        binding.bmiTextView.text = getString(R.string.bmi_x, result.bmi)
+        binding.kategoriTextView.text = getString(R.string.kategori_x,
+            getKategoriLabel(result.kategori))
+    }
+
 }
---
+++ app/src/main/java/org/d3if0114_mobpro1/model/HasilBmi.kt
@@ -0,0 +1,6 @@
+package org.d3if0114_mobpro1.model
+
+data class HasilBmi(
+    val bmi: Float,
+    val kategori: KategoriBmi
+)
