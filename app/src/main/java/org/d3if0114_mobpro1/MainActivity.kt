042fbd8b9a28b73370134ad505d6c64750be319c
Abdul Mughny Raihan <ramughny@student.telkomuniversity.ac.id>
2023-04-07 02:12:54 +0700

    Memindahkan kode logic ke view model

--- app/src/main/java/org/d3if0114_mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if0114_mobpro1/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import org.d3if0114_mobpro1.databinding.ActivityMainBinding
 import org.d3if0114_mobpro1.model.HasilBmi
 import org.d3if0114_mobpro1.model.KategoriBmi
@@ -11,6 +12,9 @@ import org.d3if0114_mobpro1.model.KategoriBmi
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +22,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener { hitungBmii() }
+        binding.button.setOnClickListener { hitungBmi() }
     }
 
-    private fun hitungBmii() {
+    private fun hitungBmi() {
         val berat = binding.beratEditText.text.toString()
         if (TextUtils.isEmpty(berat)) {
             Toast.makeText(this, R.string.berat_invalid, Toast.LENGTH_LONG).show()
@@ -38,7 +42,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, R.string.gender_invalid, Toast.LENGTH_LONG).show()
             return
         }
-        val result = hitungBmi(
+        val result = viewModel.hitungBmi(
             berat.toFloat(),
             tinggi.toFloat(),
             selectedId == R.id.priaRadioButton
@@ -46,29 +50,6 @@ class MainActivity : AppCompatActivity() {
         showResult(result)
     }
 
-    private fun hitungBmi(berat: Float, tinggi: Float, isMale: Boolean): HasilBmi {
-        val tinggiCm = tinggi / 100
-        val bmi = berat / (tinggiCm * tinggiCm)
-        val kategori = getKategori(bmi, isMale)
-        return HasilBmi(bmi, kategori)
-    }
-
-    private fun getKategori(bmi: Float, isMale: Boolean): KategoriBmi {
-        val kategori = if (isMale) {
-            when {
-                bmi < 20.5 -> KategoriBmi.KURUS
-                bmi >= 27.0 ->  KategoriBmi.GEMUK
-                else ->  KategoriBmi.IDEAL
-            }
-        } else {
-            when {
-                bmi < 18.5 -> KategoriBmi.KURUS
-                bmi >= 25.0 -> KategoriBmi.GEMUK
-                else -> KategoriBmi.IDEAL
-            }
-        }
-        return kategori
-    }
 
     private fun getKategoriLabel(kategori: KategoriBmi): String {
         val stringRes = when (kategori) {
